32294485c38d803ba8e8b7aa9d2eeb232e6ffb00
abc <abc@example.com>
2020-07-05 07:33:36 +0800

    base

--- a.java
+++ a.java
@@ -1,4 +1,18 @@
 package order_java;
+
+import javax.swing.*;
 public class a{
 
+    public static void main(String args[]){
+        JFrame frame = new JFrame("Custom T-Shirt Shop");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+
+        JButton button1 = new JButton("Staff Member");
+        JButton button1 = new JButton("Customer");
+
+        frame.getContentPane().add()
+        frame.getContentPane().add(button1);
+        frame.setVisible(true);
+     }
 }
