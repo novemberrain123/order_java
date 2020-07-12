31f7e2aa92e2c6b07471e7be2fd42f3b63d2e9be
novemberrain123 <pleaseworkusob@gmail.com>
2020-07-12 18:42:15 +0800

    added home market page

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 package order_java;
-
 import java.awt.*;
 import javax.swing.*;
-import Windows;
 
 public class Main{
 
     public static void main(String args[]){
-        Windows.createShowGUI(1);
-        Windows.createShowGUI(2);
+        JFrame frame = Windows.createShowGUI(2);
+        PageMarket.addComponentsToMarketHomePane(frame.getContentPane());
+        frame.setVisible(true);
      }
 }
