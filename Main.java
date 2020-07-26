6b43ddc42e2be056b6612b74fc2698ba8243c4e3
Lim Jun Shen <lengzai_roxes@hotmail.com>
2020-07-26 16:10:48 +0800

    paymethodpage

--- Main.java
+++ Main.java
@@ -5,8 +5,11 @@ import javax.swing.*;
 public class Main{
 
     public static void main(String args[]){
-        JFrame frame = Windows.createShowGUI(1);
+        JFrame frame = Windows.createShowGUI(2);
+        PageMembership.addComponentsToMembershipPane(frame.getContentPane());
         frame.setVisible(true);
-
+        JFrame frame1 = Windows.createShowGUI(2);
+        PagePayMethod.addComponentsToPayMethodPane(frame1.getContentPane());
+        frame.setVisible(true);
      }
 }
