40defd12e44fc1346666de2b6e31b127c758f60c
john <john@localhost.localdomain>
2020-08-18 21:36:55 +0800

    Fix small bug

--- src/order_java/Main.java
+++ src/order_java/Main.java
@@ -4,12 +4,6 @@ import order_java.GUI.*;
 public class Main{
 
     public static void main(String args[]){
-<<<<<<< HEAD:Main.java
-        Windows.createShowGUI(1);
-        Windows.createShowGUI(2);
-        System.out.print("Hi");
-=======
         MiscFunctions.generateDefaultFrame();
->>>>>>> 0160f879cc9f8551cb8732a9d10b79b55a834ffa:src/order_java/Main.java
      }
 }
